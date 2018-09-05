d733e6bbedf9cde1f5966eaa07263406a03f7664
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2018-09-05 18:05:58 +0200

    save some ecn work

--- plugins/ecn/bpf.h
+++ plugins/ecn/bpf.h
@@ -5,8 +5,12 @@
 typedef struct {
     /* ECN Data */
     uint32_t ecn_val;
+    uint32_t ecn_sock_flags;
     uint64_t ecn_ect0_marked_pkts;
     uint64_t ecn_ect1_marked_pkts;
     uint64_t ecn_ect_ce_marked_pkts;
-    uint32_t ecn_sock_flags;
+    uint64_t ecn_ect0_remote_pkts;
+    uint64_t ecn_ect1_remote_pkts;
+    uint64_t ecn_ect_ce_remote_pkts;
+    uint64_t ecn_ack_ce_counter;
 } bpf_data;
