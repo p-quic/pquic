2a1c5898bf749b02d2185c3b2c809d3d9cb1df78
Maxime Piraux <maxime.piraux@uclouvain.be>
2018-10-25 16:39:06 +0200

    datagram plugins can now write messages received from the application

--- plugins/datagram/bpf.h
+++ plugins/datagram/bpf.h
@@ -5,3 +5,16 @@ typedef struct st_datagram_frame_t {
     uint64_t length;
     uint8_t * datagram_data_ptr;  /* Start of the data, not contained in the structure */
 } datagram_frame_t;
+
+static inline size_t varint_len(uint64_t val) {
+    if (val <= 64) {
+        return 1;
+    } else if (val <= 16383) {
+        return 2;
+    } else if (val <= 1073741823) {
+        return 4;
+    } else if (val <= 4611686018427387903) {
+        return 8;
+    }
+    return 0;
+}
--- plugins/datagram/send_datagram.c
+++ plugins/datagram/send_datagram.c
@@ -6,5 +6,42 @@
 
 protoop_arg_t send_datagram_frame(picoquic_cnx_t* cnx)
 {
+    PROTOOP_PRINTF(cnx, "send_datagram_frame called \n");
+    char *payload = (char *) cnx->protoop_inputv[0];
+    size_t len = (size_t) cnx->protoop_inputv[1];
+
+    reserve_frame_slot_t *slot = (reserve_frame_slot_t *) my_malloc(cnx, sizeof(reserve_frame_slot_t));
+    if (slot == NULL) {
+        PROTOOP_PRINTF(cnx, "Unable to allocate frame slot!\n");
+        return 1;
+    }
+    slot->frame_type = 0x1c;
+    slot->frame_ctx = my_malloc(cnx, len);
+    if (slot->frame_ctx == NULL) {
+        PROTOOP_PRINTF(cnx, "Unable to allocate frame slot!\n");
+        return 1;
+    }
+    slot->nb_bytes = 1 + varint_len(len) + len;  // Unfortunately we are always forced to account for the length field
+
+    struct iovec* message = my_malloc(cnx, sizeof(struct iovec));
+    if (message == NULL) {
+        PROTOOP_PRINTF(cnx, "Unable to allocate frame slot!\n");
+        my_free(cnx, slot);
+        return 1;
+    }
+    message->iov_base = my_malloc(cnx, len);
+    message->iov_len = len;
+    if (message->iov_base == NULL) {
+        PROTOOP_PRINTF(cnx, "Unable to allocate frame slot!\n");
+        my_free(cnx, slot);
+        return 1;
+    }
+    my_memcpy(message->iov_base, payload, slot->nb_bytes);
+    slot->frame_ctx = message;
+    size_t reserved_size = reserve_frames(cnx, 1, slot);
+    if (reserved_size <= 0) {
+        PROTOOP_PRINTF(cnx, "Unable to reserve frame slot\n");
+        return 1;
+    }
     return 0;
 }
