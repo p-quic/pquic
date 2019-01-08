4e79ca2b12f88d4d4be9441418387e2aef489d66
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2019-01-08 10:58:14 +0100

    update test run time after new reno fix
    Based on commit 5ac25e5cfbd119a167f5a98d9fbb50e272a3899b

--- picoquictest/tls_api_test.c
+++ picoquictest/tls_api_test.c
@@ -1286,12 +1286,12 @@ int tls_api_very_long_max_test()
 
 int tls_api_very_long_with_err_test()
 {
-    return tls_api_one_scenario_test(test_scenario_very_long, sizeof(test_scenario_very_long), 0x30000, 128000, 0, 0, 5000000, NULL);
+    return tls_api_one_scenario_test(test_scenario_very_long, sizeof(test_scenario_very_long), 0x30000, 128000, 0, 0, 2100000, NULL);
 }
 
 int tls_api_very_long_congestion_test()
 {
-    return tls_api_one_scenario_test(test_scenario_very_long, sizeof(test_scenario_very_long), 0, 128000, 20000, 0, 1500000, NULL);
+    return tls_api_one_scenario_test(test_scenario_very_long, sizeof(test_scenario_very_long), 0, 128000, 20000, 0, 1000000, NULL);
 }
 
 int unidir_test()
