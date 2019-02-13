bb311a0a511b322ae77de1c7e3069604d0e5bc01
François Michel <francois.michel@uclouvain.be>
2019-02-13 18:27:27 +0100

    FEC: fix underflow when choosing the symbols to protect

--- plugins/fec/framework_protoops/window_protect_all_inflight_source_symbols.c
+++ plugins/fec/framework_protoops/window_protect_all_inflight_source_symbols.c
@@ -8,7 +8,7 @@ protoop_arg_t window_select_symbols_to_protect(picoquic_cnx_t *cnx)
     window_fec_framework_t *wff = (window_fec_framework_t *) get_cnx(cnx, CNX_AK_INPUT, 1);
     fb->current_source_symbols = 0;
     PROTOOP_PRINTF(cnx, "SELECT, SMALLEST = %u, HIGHEST = %u\n", wff->smallest_in_transit, wff->highest_in_transit);
-    for (int i = MAX(wff->smallest_in_transit, wff->highest_in_transit - RECEIVE_BUFFER_MAX_LENGTH) ; i <= wff->highest_in_transit ; i++) {
+    for (int i = MAX(wff->smallest_in_transit, wff->highest_in_transit - MIN(RECEIVE_BUFFER_MAX_LENGTH, wff->highest_in_transit)) ; i <= wff->highest_in_transit ; i++) {
         source_symbol_t *ss = wff->fec_window[((uint32_t) i) % RECEIVE_BUFFER_MAX_LENGTH];
         if (ss && ss->source_fec_payload_id.raw == i) {
             fb->source_symbols[fb->current_source_symbols++] = ss;
