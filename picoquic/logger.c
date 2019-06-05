f8b121a37050f8db62572dabacc3fc2971f07d78
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2019-06-05 15:05:12 +0200

    don't die if F is null

--- picoquic/logger.c
+++ picoquic/logger.c
@@ -1836,18 +1836,20 @@ void picoquic_log_transport_extension(FILE* F, picoquic_cnx_t* cnx, int log_cnxi
 
 void picoquic_log_congestion_state(FILE* F, picoquic_cnx_t* cnx, uint64_t current_time)
 {
-    picoquic_path_t * path_x = cnx->path[0];
+    if (F != NULL) {
+        picoquic_path_t * path_x = cnx->path[0];
 
-    fprintf(F, "%" PRIx64 ": ", picoquic_val64_connection_id(picoquic_get_logging_cnxid(cnx)));
-    picoquic_log_time(F, cnx, current_time, "T= ", ", ");
-    fprintf(F, "cwin: %d,", (int)path_x->cwin);
-    fprintf(F, "flight: %d,", (int)path_x->bytes_in_transit);
-    fprintf(F, "nb_ret: %d,", (int)cnx->nb_retransmission_total);
-    fprintf(F, "rtt_min: %d,", (int)path_x->rtt_min);
-    fprintf(F, "rtt: %d,", (int)path_x->smoothed_rtt);
-    fprintf(F, "rtt_var: %d,", (int)path_x->rtt_variant);
-    fprintf(F, "max_ack_delay: %d,", (int)path_x->max_ack_delay);
-    fprintf(F, "state: %d\n", (int)cnx->cnx_state);
+        fprintf(F, "%" PRIx64 ": ", picoquic_val64_connection_id(picoquic_get_logging_cnxid(cnx)));
+        picoquic_log_time(F, cnx, current_time, "T= ", ", ");
+        fprintf(F, "cwin: %d,", (int)path_x->cwin);
+        fprintf(F, "flight: %d,", (int)path_x->bytes_in_transit);
+        fprintf(F, "nb_ret: %d,", (int)cnx->nb_retransmission_total);
+        fprintf(F, "rtt_min: %d,", (int)path_x->rtt_min);
+        fprintf(F, "rtt: %d,", (int)path_x->smoothed_rtt);
+        fprintf(F, "rtt_var: %d,", (int)path_x->rtt_variant);
+        fprintf(F, "max_ack_delay: %d,", (int)path_x->max_ack_delay);
+        fprintf(F, "state: %d\n", (int)cnx->cnx_state);
+    }
 }
 
 /*
