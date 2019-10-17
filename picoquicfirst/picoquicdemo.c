d4bd7e4b34daaac45e2ebc50e6e2418f29ffc52a
Maxime Piraux <maxime.piraux@uclouvain.be>
2019-10-17 16:41:48 +0200

    Fixes a buggy fprintf and NS3 CMake env var

--- picoquicfirst/picoquicdemo.c
+++ picoquicfirst/picoquicdemo.c
@@ -1327,7 +1327,7 @@ int quic_client(const char* ip_address_text, int server_port, const char * sni,
         uint8_t* ticket;
         uint16_t ticket_length;
 
-        if (sni != NULL && 0 == picoquic_get_ticket(qclient->p_first_ticket, picoquic_current_time(), sni, (uint16_t)strlen(sni), alpn, (uint16_t)strlen(alpn), &ticket, &ticket_length)) {
+        if (sni != NULL && 0 == picoquic_get_ticket(qclient->p_first_ticket, picoquic_current_time(), sni, (uint16_t)strlen(sni), alpn, (uint16_t)strlen(alpn), &ticket, &ticket_length) && F_log) {
             fprintf(F_log, "Received ticket from %s:\n", sni);
             picoquic_log_picotls_ticket(F_log, picoquic_null_connection_id, ticket, ticket_length);
         }
@@ -1642,9 +1642,6 @@ int main(int argc, char** argv)
         }
     }
 
-    if (!F_log && (!log_file || strcmp(log_file, "/dev/null") != 0)) {
-        F_log = stdout;
-    }
 
     FILE* F_tls_secrets = NULL;
 
