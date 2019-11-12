b9c44813b8880b369601c9b84f904df136570881
François Michel <francois.michel@uclouvain.be>
2019-11-12 22:35:11 +0100

    add westwood congestion control

---
+++ plugins/westwood/westwood.h
@@ -0,0 +1,121 @@
+#ifndef WESTWOOD_H
+#define WESTWOOD_H
+
+#include <picoquic.h>
+
+#include "../../picoquic/memcpy.h"
+#include "../../picoquic/memory.h"
+#include "../helpers.h"
+#include "../../picoquic/picoquic.h"
+
+#define WESTWOOD_OPAQUE_ID 0x0F
+#define NB_RTT_WESTWOOD 4
+
+typedef enum {
+    westwood_alg_slow_start = 0,
+    westwood_alg_congestion_avoidance
+} westwood_alg_state_t;
+
+typedef struct {
+    westwood_alg_state_t alg_state;
+    uint64_t ssthresh;
+    int64_t residual_ack;
+    int64_t recovery_start;
+    int64_t cwin_before_recovery_start;
+    int64_t min_rtt;
+    int64_t nb_rtt;
+    uint64_t last_rtt[NB_RTT_WESTWOOD];
+    int64_t bytes_acknowledged_since_last_rtt;
+    int64_t bytes_acknowledged_during_previous_round;
+    int64_t last_rtt_timestamp;
+    int64_t last_rtt_value;
+
+} westwood_state_t;
+
+
+static __attribute__((always_inline)) westwood_state_t *initialize_westwood_state_t(picoquic_cnx_t *cnx, uint64_t current_time)
+{
+    westwood_state_t *state = (westwood_state_t *) my_malloc(cnx, sizeof(westwood_state_t));
+    if (!state) return NULL;
+    my_memset(state, 0, sizeof(westwood_state_t));
+
+    state->alg_state = westwood_alg_slow_start;
+    state->ssthresh = (uint64_t) ((int64_t)-1);
+    state->last_rtt_timestamp = current_time;
+    state->last_rtt_value = -1;
+    return state;
+}
+
+static __attribute__((always_inline)) westwood_state_t *get_westwood_state_t(picoquic_cnx_t *cnx, uint64_t current_time)
+{
+    int allocated = 0;
+    westwood_state_t **state_ptr = (westwood_state_t **) get_opaque_data(cnx, WESTWOOD_OPAQUE_ID, sizeof(westwood_state_t *), &allocated);
+    if (!state_ptr) return NULL;
+    if (allocated) {
+        *state_ptr = initialize_westwood_state_t(cnx, current_time);
+    }
+    return *state_ptr;
+}
+
+
+/*
+ * Reset the pacing data after CWIN is updated
+ */
+
+static __attribute__((always_inline)) void update_pacing_data(picoquic_path_t * path_x)
+{
+
+    int64_t cwin = get_path(path_x, AK_PATH_CWIN, 0);
+    int64_t send_mtu = get_path(path_x, AK_PATH_SEND_MTU, 0);
+    int64_t smoothed_rtt = get_path(path_x, AK_PATH_SMOOTHED_RTT, 0);
+    int64_t rtt_min = get_path(path_x, AK_PATH_RTT_MIN, 0);
+
+
+    int64_t packet_time_nano_sec = smoothed_rtt * 1000ull * send_mtu;
+    packet_time_nano_sec /= (uint64_t) cwin;
+    set_path(path_x, AK_PATH_PACKET_TIME_NANO_SEC, 0, packet_time_nano_sec);
+
+    int64_t pacing_margin_micros = 16 * packet_time_nano_sec;
+    if (pacing_margin_micros > (rtt_min / 8)) {
+        pacing_margin_micros = (rtt_min / 8);
+    }
+    if (pacing_margin_micros < 1000) {
+        pacing_margin_micros = 1000;
+    }
+
+    set_path(path_x, AK_PATH_PACING_MARGIN_MICROS, 0, pacing_margin_micros);
+
+}
+
+
+static __attribute__((always_inline)) void westwood_enter_recovery(picoquic_cnx_t *cnx, picoquic_path_t* path_x,
+                                            picoquic_congestion_notification_t notification,
+                                            westwood_state_t* westwood_state,
+                                            uint64_t current_time, uint64_t *cwin)
+{
+
+    westwood_state->ssthresh = 0;
+    // westwood cwin reduction formula
+    if (westwood_state->last_rtt_value != -1)
+        westwood_state->ssthresh = westwood_state->bytes_acknowledged_during_previous_round*westwood_state->min_rtt/(uint64_t) westwood_state->last_rtt_value;
+    westwood_state->cwin_before_recovery_start = *cwin;
+    if (westwood_state->ssthresh < PICOQUIC_CWIN_MINIMUM) {
+        westwood_state->ssthresh = PICOQUIC_CWIN_MINIMUM;
+    }
+
+    if (notification == picoquic_congestion_notification_timeout) {
+        *cwin = PICOQUIC_CWIN_MINIMUM;
+        westwood_state->alg_state = westwood_alg_slow_start;
+    } else {
+        *cwin = westwood_state->ssthresh;
+        westwood_state->alg_state = westwood_alg_congestion_avoidance;
+    }
+
+    westwood_state->recovery_start = current_time;
+
+    westwood_state->residual_ack = 0;
+
+
+}
+
+#endif // WESTWOOD_H
---
+++ plugins/westwood/westwood_notify.c
@@ -0,0 +1,137 @@
+
+#include <picoquic.h>
+#include <getset.h>
+#include "westwood.h"
+
+
+
+
+
+/**
+ * See PROTOOP_NOPARAM_CONGESTION_ALGORITHM_NOTIFY
+ */
+protoop_arg_t congestion_algorithm_notify(picoquic_cnx_t *cnx)
+{
+    picoquic_path_t* path_x = (picoquic_path_t *) get_cnx(cnx, AK_CNX_INPUT, 0); //(picoquic_path_t*) cnx->protoop_inputv[0];
+    picoquic_congestion_notification_t notification = (picoquic_congestion_notification_t) get_cnx(cnx, AK_CNX_INPUT, 1);
+    uint64_t rtt_measurement = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 2);
+    uint64_t nb_bytes_acknowledged = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 3);
+    uint64_t lost_packet_number = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 4);
+    uint64_t current_time = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 5);
+
+    westwood_state_t* westwood_state = (westwood_state_t*) get_westwood_state_t(cnx, current_time);
+
+    uint64_t cwin = get_path(path_x, AK_PATH_CWIN, 0);
+    uint64_t bytes_in_transit = get_path(path_x, AK_PATH_BYTES_IN_TRANSIT, 0);
+    int64_t send_mtu = get_path(path_x, AK_PATH_SEND_MTU, 0);
+    int64_t smoothed_rtt = get_path(path_x, AK_PATH_SMOOTHED_RTT, 0);
+
+    if (westwood_state != NULL) {
+        switch (notification) {
+            case picoquic_congestion_notification_acknowledgement: {
+                if ((int64_t) current_time - (int64_t) westwood_state->last_rtt_timestamp > smoothed_rtt) {
+                    // we're in a new round
+                    westwood_state->bytes_acknowledged_during_previous_round = westwood_state->bytes_acknowledged_since_last_rtt;
+                    westwood_state->bytes_acknowledged_since_last_rtt = 0;
+                    westwood_state->last_rtt_timestamp = current_time;
+                    westwood_state->last_rtt_value = smoothed_rtt;
+                }
+                westwood_state->bytes_acknowledged_since_last_rtt += nb_bytes_acknowledged;
+                switch (westwood_state->alg_state) {
+                    case westwood_alg_slow_start:
+                        /* Only increase when the app is CWIN limited */
+                        if (cwin <= bytes_in_transit + nb_bytes_acknowledged) {
+                            cwin += nb_bytes_acknowledged;
+                            /* if cnx->cwin exceeds SSTHRESH, exit and go to CA */
+                            if (cwin >= westwood_state->ssthresh) {
+                                westwood_state->alg_state = westwood_alg_congestion_avoidance;
+                            }
+                        }
+                        break;
+                    case westwood_alg_congestion_avoidance:
+                    default: {
+                        // we increase the cwin by nb_bytes_acknowledged per RTT
+                        uint64_t complete_delta = nb_bytes_acknowledged * send_mtu + westwood_state->residual_ack;
+                        westwood_state->residual_ack = complete_delta % (uint64_t) cwin;
+                        cwin += complete_delta / (uint64_t) cwin;
+                        break;
+                    }
+                }
+                break;
+            }
+            case picoquic_congestion_notification_repeat:
+            case picoquic_congestion_notification_timeout:
+                /* enter recovery */
+                if (current_time - westwood_state->recovery_start > smoothed_rtt) {
+                    westwood_enter_recovery(cnx, path_x, notification, westwood_state, current_time, &cwin);
+                }
+                break;
+            case picoquic_congestion_notification_spurious_repeat:
+                if (current_time - westwood_state->recovery_start < smoothed_rtt) {
+                    /*
+                     * If spurious repeat of initial loss detected,
+                     * exit recovery and reset threshold to pre-entry cwin.
+                     */
+
+                    if (cwin < westwood_state->cwin_before_recovery_start) {
+                        cwin = westwood_state->cwin_before_recovery_start;
+                        westwood_state->alg_state = westwood_alg_congestion_avoidance;
+                    }
+
+                    /*
+                     * Reno was doing this (reset the cwin to the state before recovery) :
+                     */
+//                    if (cwin < 2 * westwood_state->ssthresh) {
+//                        cwin = 2 * westwood_state->ssthresh;
+//                        westwood_state->alg_state = westwood_alg_congestion_avoidance;
+//                    }
+                }
+                break;
+            case picoquic_congestion_notification_rtt_measurement:
+                /* Using RTT increases as signal to get out of initial slow start */
+                if (westwood_state->alg_state == westwood_alg_slow_start &&
+                    westwood_state->ssthresh == (uint64_t) ((int64_t)-1)) {
+                    uint64_t rolling_min;
+                    uint64_t delta_rtt;
+
+                    if (rtt_measurement < westwood_state->min_rtt || westwood_state->min_rtt == 0) {
+                        westwood_state->min_rtt = rtt_measurement;
+                    }
+
+                    if (westwood_state->nb_rtt > NB_RTT_WESTWOOD) {
+                        westwood_state->nb_rtt = 0;
+                    }
+
+                    westwood_state->last_rtt[westwood_state->nb_rtt] = rtt_measurement;
+                    westwood_state->nb_rtt++;
+
+                    rolling_min = westwood_state->last_rtt[0];
+
+                    for (int i = 1; i < NB_RTT_WESTWOOD; i++) {
+                        if (westwood_state->last_rtt[i] == 0) {
+                            break;
+                        }
+                        else if (westwood_state->last_rtt[i] < rolling_min) {
+                            rolling_min = westwood_state->last_rtt[i];
+                        }
+                    }
+
+                    delta_rtt = rolling_min - westwood_state->min_rtt;
+                    if (delta_rtt * 4 > westwood_state->min_rtt) {
+                        /* RTT increased too much, get out of slow start! */
+                        westwood_state->alg_state = westwood_alg_congestion_avoidance;
+                    }
+                }
+                break;
+            default:
+                /* ignore */
+                break;
+        }
+    }
+
+    /* Compute pacing data */
+    update_pacing_data(path_x);
+    set_path(path_x, AK_PATH_CWIN, 0, cwin);
+    set_path(path_x, AK_PATH_BYTES_IN_TRANSIT, 0, bytes_in_transit);
+    return 0;
+}
