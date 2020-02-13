efd7d6763abca91c50609af076bf4dadf73c8542
Maxime Piraux <maxime.piraux@uclouvain.be>
2020-02-13 15:19:54 +0100

    Fixes datagram congestion control switch

--- plugins/datagram/bpf.h
+++ plugins/datagram/bpf.h
@@ -115,7 +115,6 @@ static __attribute__((always_inline)) protoop_arg_t send_datagram_to_application
     slot->frame_type = 0x60;
     slot->nb_bytes = 1;
     slot->frame_ctx = NULL;
-    slot->is_congestion_controlled = DCC;
     reserve_frames(cnx, 1, slot);
     if (frame->datagram_id != 0) {
         m->expected_datagram_id = frame->datagram_id + 1;
