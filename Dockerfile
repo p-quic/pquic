9876f7495f376fa78fe90585ac2468bdb77b1fa3
Maxime Piraux <maxime.piraux@uclouvain.be>
2020-08-14 14:27:24 +0200

    Adds Dockerfiles for building, running and interoping PQUIC

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+# Docker image for building and running PQUIC
+FROM ubuntu:20.04
+
+ENV TZ=Europe/Brussels
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+RUN apt-get update && \
+    apt-get install -y build-essential git cmake \
+    openssl libssl-dev libarchive-dev google-perftools libgoogle-perftools-dev pkg-config clang llvm && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN ln -s /usr/bin/clang /usr/bin/clang-6.0 && \
+    ln -s /usr/bin/llc /usr/bin/llc-6.0
+
+RUN mkdir /src
+WORKDIR /src
+
+RUN echo install Test::TCP | perl -MCPAN -
+RUN echo install Scope::Guard | perl -MCPAN -
+
+RUN git clone https://github.com/h2o/picotls.git && \
+    cd picotls && \
+    git submodule init && \
+    git submodule update && \
+    cmake . && \
+    make && \
+    make check
+
+
+RUN mkdir /src/pquic
+COPY . /src/pquic/
+WORKDIR /src/pquic/
+
+RUN git submodule init && \
+    git submodule update
+
+RUN cd picoquic/michelfralloc && make
+
+RUN cmake . && make
+
+RUN cd plugins && make
---
+++ interop/run_endpoint.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Set up the routing needed for the simulation
+/setup.sh
+
+# The following variables are available for use:
+# - ROLE contains the role of this execution context, client or server
+# - SERVER_PARAMS contains user-supplied command line parameters
+# - CLIENT_PARAMS contains user-supplied command line parameters
+
+case "$TESTCASE" in
+    "versionnegotiation") ;;
+    "handshake") ;;
+    "transfer") ;;
+    "resumption") ;;
+    "zerortt") ;;
+    "http3") ;;
+    "multiconnect") ;;
+    *) echo "Unsupported test case: $TESTCASE"; exit 127 ;;
+esac
+
+if [ "$ROLE" == "client" ]; then
+    # Wait for the simulator to start up.
+    /wait-for-it.sh sim:57832 -s -t 30
+    TEST_PARAMS="$CLIENT_PARAMS -o /downloads"
+    TEST_PARAMS="$TEST_PARAMS -X /logs/keys.log"
+
+    echo "Requests: " $REQUESTS
+    for REQ in $REQUESTS; do
+        FILE=`echo $REQ | cut -f4 -d'/'`
+        echo "parsing <$REQ> as <$FILE>"
+        FILELIST=${FILELIST}"-:/"${FILE}";"
+    done
+
+    if [ "$TESTCASE" == "http3" ]; then
+        TEST_PARAMS="$TEST_PARAMS -a h3-29"
+    elif [ "$TESTCASE" == "versionnegotiation" ]; then
+        TEST_PARAMS="$TEST_PARAMS -v 5a6a7a8a";
+    else
+        TEST_PARAMS="$TEST_PARAMS -v ff00001d";
+    fi
+
+    if [ "$TESTCASE" == "resumption" ] ||
+       [ "$TESTCASE" == "zerortt" ] ; then
+            TEST_PARAMS="$TEST_PARAMS -a hq-29"
+            FILE1=`echo $FILELIST | cut -f1 -d";"`
+            FILE2=`echo $FILELIST | cut -f2- -d";"`
+            L1="/logs/first_log.txt"
+            L2="/logs/second_log.txt"
+            rm *.bin
+            /picoquicdemo $TEST_PARAMS server 443 $FILE1 > $L1
+            if [ $? != 0 ]; then
+                RET=1
+                echo "First call to picoquicdemo failed"
+            else
+                /picoquicdemo $TEST_PARAMS server 443 $FILE2 > $L2
+                if [ $? != 0 ]; then
+                    RET=1
+                    echo "Second call to picoquicdemo failed"
+                fi
+            fi
+    elif [ "$TESTCASE" == "multiconnect" ]; then
+        for CREQ in $REQUESTS; do
+            CFILE=`echo $CREQ | cut -f4 -d'/'`
+            CFILEX="/$CFILE"
+            MCLOG="/logs/mc-$CFILE.txt"
+            /picoquicdemo $TEST_PARAMS server 443 $CFILEX > $MCLOG
+            if [ $? != 0 ]; then
+                RET=1
+                echo "Call to picoquicdemo failed"
+            fi
+        done
+    else
+        /picoquicdemo $TEST_PARAMS server 443 $FILELIST
+    fi
+elif [ "$ROLE" == "server" ]; then
+    TEST_PARAMS="$SERVER_PARAMS -k /certs/key.pem"
+    TEST_PARAMS="$TEST_PARAMS -c /certs/cert.pem"
+    TEST_PARAMS="$TEST_PARAMS -p 443"
+    TEST_PARAMS="$TEST_PARAMS -X /logs/keys.log"
+    TEST_PARAMS="$TEST_PARAMS -w /www"
+    /picoquicdemo $TEST_PARAMS
+fi
